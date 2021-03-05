465f4a9c3a7d31682f8eff6fb3b600fd2c64368a
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-03-05 12:53:11 +0100

    Added sub command

--- src/main/java/ch/heig_vd/app/Main.java
+++ src/main/java/ch/heig_vd/app/Main.java
@@ -2,15 +2,32 @@ package ch.heig_vd.app;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
 
-@Command(name = "gen")
+@Command(name = "gen", mixinStandardHelpOptions = true)
 public class Main implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "The command to execute") private String param;
+
 
     @Override
     public Integer call() {
-        System.out.println("Please use one of the subcommands: 'new', 'clean', 'build' or 'serve'");
+        switch (param){
+            case "clean":
+                break;
+            case "build":
+                break;
+            case "new":
+                break;
+            case "serve":
+                break;
+            default: System.out.println("Please use one of the subcommands: 'new', 'clean', 'build' or 'serve'");
+        }
+
+
         return 0;
     }
 
