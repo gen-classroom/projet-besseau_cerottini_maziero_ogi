429b8e5945835a2b887e579fc19f9f3d93cda4b2
MazieroMarco <maziero.marco@gmail.com>
2021-03-24 19:02:48 +0100

    Started metadata parser feature

---
+++ src/main/java/ch/heig_vd/app/utils/MetadataParser.java
@@ -0,0 +1,9 @@
+package ch.heig_vd.app.utils;
+
+import java.io.File;
+
+public class MetadataParser {
+    public static Metadata ExctractMetadata(File file) {
+        return new Metadata("", "", "");
+    }
+}
