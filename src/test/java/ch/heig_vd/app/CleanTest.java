d3f98c400ec0d183734b676367db4739c66ba792
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-03-24 22:24:25 +0100

    corrected tests

--- src/test/java/ch/heig_vd/app/CleanTest.java
+++ src/test/java/ch/heig_vd/app/CleanTest.java
@@ -15,23 +15,25 @@ public class CleanTest {
     public void cleanShouldDeleteBuildFolderWithRelativePath() throws IOException {
         String path1 = "./mon/site/";
         String path2 = "mon/site/";
+
         File directory = new File(path1 + "/build");
         directory.mkdirs();
-        new CommandLine(new Main()).execute("statique", "Clean", path1);
+        new CommandLine(new Main()).execute("statique", "clean", path1);
         assertFalse(directory.exists());
         directory = new File(path2 + "/build");
         directory.mkdirs();
-        new CommandLine(new Main()).execute("statique", "Clean", path2);
+        new CommandLine(new Main()).execute("statique", "clean", path2);
         assertFalse(directory.exists());
     }
 
     @Test
     public void cleanShouldDeleteBuildFolderWithAbsolutePath() throws IOException {
         String path = "/mon/site/";
-        File directory = new File(path + "/build/");
+        String pwd = new File(".").getCanonicalPath();
+        File directory = new File(pwd+"/"+path + "/build/");
         directory.delete();
         if (directory.mkdirs()) {
-            new CommandLine(new Main()).execute("statique", "Clean");
+            new CommandLine(new Main()).execute("statique","clean", pwd+"/"+path );
             assertFalse(directory.exists());
         } else {
             System.err.println("Could not test for absolute path");
@@ -41,7 +43,7 @@ public class CleanTest {
     @Test
     public void cleanShouldThrowsWhenGivenAnNonExistentPath() throws IOException {
         String path = "/should/not/exists/";
-        assertEquals(2, new CommandLine(new Main()).execute("statique", "Clean", path));
+        assertEquals(2, new CommandLine(new Main()).execute("statique", "clean", path));
 
 
     }
