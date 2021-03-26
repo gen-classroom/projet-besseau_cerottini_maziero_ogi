db493cb717a9f1fb03237ddddfc5fd724aa8e207
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-03-26 13:50:11 +0100

    added test

--- src/test/java/ch/heig_vd/app/utils/ConverterTest.java
+++ src/test/java/ch/heig_vd/app/utils/ConverterTest.java
@@ -99,6 +99,54 @@ public class ConverterTest {
         conv.MarkdownToHTML(file, "./");
     }
 
+    @Test
+    public void mdFileWithNoValidMetadataShouldBeRejected() {
+        try {
+            // Creates a test json config file
+            File jsonConfig = new File("./conf.json");
+            BufferedWriter jsonWriter = new BufferedWriter(new FileWriter(jsonConfig));
+            jsonWriter.write("{\n" +
+                    "\"title\":\"a\",\n" +
+                    "\"b\":\"c\"}");
+            jsonWriter.flush();
+            jsonWriter.close();
+
+            // Creates test md file
+            File input = new File("./input.md");
+            FileWriter writer = new FileWriter(input);
+            String inputContent = "titre:metaTitle\n" +
+                    "date:metaDate\n" +
+                    "---\n" +
+                    "This is *Sparta*";
+            writer.write(inputContent);
+            writer.close();
+
+            // Records console
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(os);
+            PrintStream sysOut = System.err;
+            System.setOut(ps);
+
+            // Converts
+            Converter conv = new Converter(jsonConfig);
+            conv.MarkdownToHTML(input, "./");
+
+            // Checks console
+            String expected = "File input.md could not be" +
+                    "parsed and was not added to destination\n" +
+                    "Error : Metadata are incomplete";
+            assertEquals(expected, os.toString());
+            System.out.flush();
+
+            // Resets the system output
+            System.setErr(sysOut);
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @AfterClass
     public static void cleanUp() {
         File file = new File("testFile.txt");
