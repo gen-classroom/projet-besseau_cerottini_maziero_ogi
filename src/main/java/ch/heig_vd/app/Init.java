78d95c113e5625677ea8dc70360d1f24f7b768f5
Nicolas Ogi <nicolas.ogi@heig-vd.ch>
2021-03-26 12:32:08 +0100

    Adding working tests

--- src/main/java/ch/heig_vd/app/Init.java
+++ src/main/java/ch/heig_vd/app/Init.java
@@ -6,6 +6,8 @@ import picocli.CommandLine;
 import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,18 +15,20 @@ import java.util.Date;
 
 // Classe définissant la commande Init permettant de créer les dossiers du site web ainsi qu'un exemple de fichier
 // config.json et index.md
-@CommandLine.Command(name = "Init")
+@CommandLine.Command(name = "init", exitCodeOnExecutionException = 2)
 class Init implements Runnable {
 
-    private final static String FILEPATH_JSON = "./mon/site/config.json";
-    private final static String FILEPATH_MD = "./mon/site/index.md";
+    @CommandLine.Parameters(index = "0")
+    String filePath; // Picocli met l'argument venant après init dans cette variable
 
     DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
     Date date = new Date();
 
     public void run() {
 
-        File dir = new File("./mon/site/");
+        Path path = Paths.get(filePath).normalize().toAbsolutePath();
+
+        File dir = new File(path.toString());
 
         if (!dir.exists()) {
             dir.mkdirs();
@@ -35,23 +39,17 @@ class Init implements Runnable {
         ObjectNode rootNode = mapper.createObjectNode();
         rootNode.put("title", "Titre");
         rootNode.put("description", "Une ambiance délétère");
-        rootNode.put("domaine", "ambiance.deletere.org");
-
+        rootNode.put("domain", "ambiance.deletere.org");
 
         try {
-            // Instanciation du fichir json
-            FileWriter jsonFile = new FileWriter(FILEPATH_JSON);
+            // Création du fichir json
+            FileWriter jsonFile = new FileWriter(path.toString() + "/config.json");
             jsonFile.write(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(rootNode));
             jsonFile.close();
             System.out.println("JSON file created: " + mapper);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
             // Création du fichier Markdown
-            FileWriter mdFile = new FileWriter(FILEPATH_MD);
+            FileWriter mdFile = new FileWriter(path.toString() + "/index.md");
             mdFile.write("# Mon site trop classe !\n");
             mdFile.write("Auteur : un énorme bg\n");
             mdFile.write("Date : " + dateFormat.format(date));
--- src/test/java/ch/heig_vd/app/InitTest.java
+++ src/test/java/ch/heig_vd/app/InitTest.java
@@ -1,38 +1,52 @@
 package ch.heig_vd.app;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.Assertions;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
+
 import picocli.CommandLine;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class InitTest {
 
-    @BeforeAll
+    @BeforeClass
     public static void invokeCommandLine() {
-        new CommandLine(new Main()).execute("statique", "Init");
+        new CommandLine(new Main()).execute("statique", "init", "test_folder/mon//site");
     }
 
     @Test
     public void initShouldCreateDirectories() {
-        File dir = new File("./mon/site/");
-        assertFalse(dir.exists());
+        File dir = new File("test_folder/mon/site/");
+        assertTrue(dir.exists());
     }
 
     @Test
     public void initShouldCreateJson() {
-        File jsonFile = new File("./mon/site/config.json");
-        assertFalse(jsonFile.exists());
+        File jsonFile = new File("test_folder/mon/site/config.json");
+        assertTrue(jsonFile.exists());
     }
 
     @Test
-    public void initShouldCreateMd() throws IOException {
-        File mdFile = new File("./mon/site/index.md");
-        assertFalse(mdFile.exists());
+    public void initShouldCreateMd() {
+        File mdFile = new File("test_folder/mon/site/index.md");
+        assertTrue(mdFile.exists());
+    }
+
+    @AfterClass
+    public static void cleanAll() throws IOException {
+        Path path = Paths.get("./test_folder").normalize().toAbsolutePath();
+        System.out.println("Cleaning ALL");
+        if (!path.toFile().exists()) {
+            throw new IllegalArgumentException("Directory does not exists");
+        }
+        FileUtils.deleteDirectory(path.toFile());
+
     }
 }
