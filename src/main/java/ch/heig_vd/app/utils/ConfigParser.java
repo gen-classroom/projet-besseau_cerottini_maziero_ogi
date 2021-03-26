182d96c9c7ebd1ef4035aec72da51f62d6490df8
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-03-26 12:18:47 +0100

    added ConfigData and ConfigParser

--- src/main/java/ch/heig_vd/app/utils/ConfigParser.java
+++ src/main/java/ch/heig_vd/app/utils/ConfigParser.java
@@ -4,13 +4,31 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class ConfigParser {
-    ConfigData data;
+    private final ConfigData data;
 
-    ConfigParser(String path) throws IOException {
+    ConfigParser(File f) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
 
-        data = mapper.readValue(new File(path), ConfigData.class);
+        data = mapper.readValue(f, ConfigData.class);
+    }
+
+    public ArrayList<Metadata> getData() throws IllegalAccessException {
+        ArrayList<Metadata> out = new ArrayList<>();
+        for (Field field : ConfigData.class.getFields()){
+            out.add(new Metadata(field.getName(), field.get(data).toString()));
+        }
+
+        return out;
+    }
+
+    public static void main(String[] args) throws IOException, IllegalAccessException {
+        ConfigParser c = new ConfigParser(new File("/home/leonard/IdeaProjects/BA4/GEN/projet-besseau_cerottini_maziero_ogi/test.json"));
+        for (Metadata m : c.getData()){
+            System.out.println(m);
+        }
     }
 }
