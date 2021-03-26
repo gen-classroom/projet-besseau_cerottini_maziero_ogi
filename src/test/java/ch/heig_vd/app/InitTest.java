df26f2e124901ee9afa4b052268cad06c4434559
Nicolas Ogi <nicolas.ogi@heig-vd.ch>
2021-03-26 10:52:00 +0100

    Tests et pom.xml

--- src/test/java/ch/heig_vd/app/InitTest.java
+++ src/test/java/ch/heig_vd/app/InitTest.java
@@ -1,7 +1,10 @@
 package ch.heig_vd.app;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import picocli.CommandLine;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,20 +13,26 @@ import static org.junit.Assert.assertFalse;
 
 public class InitTest {
 
+    @BeforeAll
+    public static void invokeCommandLine() {
+        new CommandLine(new Main()).execute("statique", "Init");
+    }
 
     @Test
-    public void initShouldCreateDirectories() throws IOException {
+    public void initShouldCreateDirectories() {
         File dir = new File("./mon/site/");
         assertFalse(dir.exists());
     }
 
     @Test
-    public void initShouldCreateJson() throws IOException {
-
+    public void initShouldCreateJson() {
+        File jsonFile = new File("./mon/site/config.json");
+        assertFalse(jsonFile.exists());
     }
 
     @Test
     public void initShouldCreateMd() throws IOException {
-
+        File mdFile = new File("./mon/site/index.md");
+        assertFalse(mdFile.exists());
     }
 }
