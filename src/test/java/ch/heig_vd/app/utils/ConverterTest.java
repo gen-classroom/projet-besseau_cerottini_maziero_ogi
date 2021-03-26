9f73fbbf63ea7e7fa48a3037e265285a28863306
MazieroMarco <maziero.marco@gmail.com>
2021-03-26 13:50:39 +0100

    New test

--- src/test/java/ch/heig_vd/app/utils/ConverterTest.java
+++ src/test/java/ch/heig_vd/app/utils/ConverterTest.java
@@ -2,6 +2,7 @@ package ch.heig_vd.app.utils;
 
 import org.junit.AfterClass;
 import org.junit.Test;
+import ch.heig_vd.app.utils.Metadata;
 import org.junit.jupiter.api.AfterAll;
 
 import java.io.*;
@@ -81,53 +82,7 @@ public class ConverterTest {
         }
     }
 
-    @Test
-    public void mdFileWithNoValidMetadataShouldBeRejected() {
-        try {
-            // Creates a test json config file
-            File jsonConfig = new File("./conf.json");
-            BufferedWriter jsonWriter = new BufferedWriter(new FileWriter(jsonConfig));
-            jsonWriter.write("{\n" +
-                    "\"title\":\"a\",\n" +
-                    "\"b\":\"c\"}");
-            jsonWriter.flush();
-            jsonWriter.close();
 
-            // Creates test md file
-            File input = new File("./input.md");
-            FileWriter writer = new FileWriter(input);
-            String inputContent = "titre:metaTitle\n" +
-                    "date:metaDate\n" +
-                    "---\n" +
-                    "This is *Sparta*";
-            writer.write(inputContent);
-            writer.close();
-
-            // Records console
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            PrintStream ps = new PrintStream(os);
-            PrintStream sysOut = System.err;
-            System.setOut(ps);
-
-            // Converts
-            Converter conv = new Converter(jsonConfig);
-            conv.MarkdownToHTML(input, "./");
-
-            // Checks console
-            String expected = "File input.md could not be" +
-                    "parsed and was not added to destination\n" +
-                    "Error : Metadata are incomplete";
-            assertEquals(expected, os.toString());
-            System.out.flush();
-
-            // Resets the system output
-            System.setOut(sysOut);
-
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
-
-    }
 
     @Test(expected = RuntimeException.class)
     public void givenFileShouldNotBeDirectory() {
