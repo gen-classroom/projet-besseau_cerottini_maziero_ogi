badfc3855714df77f44fa98b3e123cda14ccad44
Insuline13 <alexandra.cerottini@heig-vd.ch>
2021-03-27 14:56:55 +0100

    changes have been made

--- src/main/java/ch/heig_vd/app/Build.java
+++ src/main/java/ch/heig_vd/app/Build.java
@@ -4,6 +4,9 @@ import picocli.CommandLine;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.apache.commons.io.FileUtils;
 
 
@@ -14,16 +17,11 @@ class Build implements Runnable {
 
     public void run() {
 
-        File directory = null;
-        try {
-            directory = new File(new File(".").getCanonicalPath()); //get current directory
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Path path = Paths.get(filePath).normalize().toAbsolutePath();
 
-        File buildDirectory = new File(directory.getPath() + filePath + "/build"); //build new directory
+        File buildDirectory = new File(path + "/build"); //build new directory
         buildDirectory.mkdir();
-        File filesDirectory = new File(directory.getPath() + filePath); //get all directory from there
+        File filesDirectory = new File(String.valueOf(path)); //get all directory from there
 
         try {
             explore(filesDirectory, buildDirectory);
@@ -44,7 +42,7 @@ class Build implements Runnable {
                     ;
                     //MarkdownToHTML(file, buildDirectory); //la fonction de Marco
                 } else if (!fileName.contains("config") && !file.isDirectory()) {
-                    File newDirectory = new File(buildDirectory + "/" + fileName); //build new directory
+                    File newDirectory = new File(buildDirectory + "/" + fileName);
                     FileUtils.copyFile(file, newDirectory);
                 } else if (file.isDirectory() && !fileName.contains("build")) {
                     File newDirectory = new File(buildDirectory + "/" + fileName); //build new directory
