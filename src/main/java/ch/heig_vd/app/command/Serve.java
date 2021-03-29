87f1e14d903df7a771005ca70a5c800a2dccd5ee
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-03-29 11:33:30 +0200

    added description to command

--- src/main/java/ch/heig_vd/app/command/Serve.java
+++ src/main/java/ch/heig_vd/app/command/Serve.java
@@ -5,6 +5,7 @@ import picocli.CommandLine;
 @CommandLine.Command(name = "serve",
         description = "Serve a static site")
 public class Serve implements Runnable{
+    @CommandLine.Parameters(description = "Path of site to serve.")
     public void run(){
         System.out.println("All good, executing Serve");
     }
