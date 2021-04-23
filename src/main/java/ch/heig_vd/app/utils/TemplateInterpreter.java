ae0f3179f3c340612886d7946df19113c9ef3c54
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-04-23 13:04:34 +0200

    Added constructor for template interpreter

--- src/main/java/ch/heig_vd/app/utils/TemplateInterpreter.java
+++ src/main/java/ch/heig_vd/app/utils/TemplateInterpreter.java
@@ -1,4 +1,18 @@
 package ch.heig_vd.app.utils;
 
+import com.github.jknack.handlebars.*;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.CompositeTemplateLoader;
+import com.github.jknack.handlebars.io.FileTemplateLoader;
+
+import java.io.File;
+
 public class TemplateInterpreter {
+    private final Handlebars handlebars;
+
+    TemplateInterpreter(File templateFile){
+        handlebars = new Handlebars(new CompositeTemplateLoader(new FileTemplateLoader(templateFile), new ClassPathTemplateLoader()));
+        handlebars.registerHelper("md", new MarkdownHelper());
+        handlebars.setPrettyPrint(true);
+    }
 }
