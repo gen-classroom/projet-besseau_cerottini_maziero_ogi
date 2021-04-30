756dd2903c4341b532c4f496ab19cf02cff7dfa1
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-05-01 00:16:26 +0200

    Added test

--- src/test/java/ch/heig_vd/app/utils/PageParserTest.java
+++ src/test/java/ch/heig_vd/app/utils/PageParserTest.java
@@ -91,6 +91,7 @@ public class PageParserTest {
         }
     }
 
+
     @Test
     public void markdownContentShouldBeParsedAndExtracted() {
         try {
@@ -115,10 +116,49 @@ public class PageParserTest {
     }
 
     @Test
-    public void markdownAndMetaShouldBeParsedAndExtracted() {
+    public void extractAllShouldThrowsInFileWithNoEndLineForMetaData() throws IOException {
+        // Creates test file
+        File input = new File("./PageParserTest/input3.md");
+        String inputContent = "titre:metaTitle\n" +
+                "auteur:metaAuthor\n" +
+                "date:metaDate\n" +
+                "# MarkdownTitle";
+        FileWriter writer = new FileWriter(input);
+        writer.write(inputContent);
+        writer.close();
+        ArrayList<Metadata> meta = new ArrayList<>();
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            String content = PageParser.extractAll(input, meta);
+        });
+        assertEquals(exception.getMessage(), "Incorrect # MarkdownTitle metadata line\nCheck end of metadata separator \"---\"");
+    }
+
+    @Test
+    public void extractAllShouldThrowsInFileWithWrongMetadataFields() throws IOException {
+            // Creates test file
+            File input = new File("./PageParserTest/input2.md");
+            String inputContent = "titre:metaTitle\n" +
+                    "auteur:metaAuthor\n" +
+                    "wrongmetadatafilednoseparator\n" +
+                    "---\n" +
+                    "# MarkdownTitle";
+            FileWriter writer = new FileWriter(input);
+            writer.write(inputContent);
+            writer.close();
+
+            ArrayList<Metadata> meta = new ArrayList<>();
+            Exception exception = assertThrows(RuntimeException.class, () -> {
+                String content = PageParser.extractAll(input, meta);
+            });
+            assertEquals(exception.getMessage(), "Incorrect wrongmetadatafilednoseparator metadata line\nCheck end of metadata separator \"---\"");
+
+    }
+
+    @Test
+    public void extractAllShouldWork() {
         try {
             // Creates test file
-            File input = new File("./PageParserTest/input4.md");
+            File input = new File("./PageParserTest/input5.md");
             String inputContent = "titre:metaTitle\n" +
                     "myMetaName:metaContentText\n" +
                     "date:metaDate\n"+
