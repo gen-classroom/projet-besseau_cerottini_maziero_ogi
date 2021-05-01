868d63bc8cfb5541a9fa0728cbaaf60e35c0d7b0
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-05-01 13:11:29 +0200

    Removed previously used functions

--- src/test/java/ch/heig_vd/app/converter/parser/PageParserTest.java
+++ src/test/java/ch/heig_vd/app/converter/parser/PageParserTest.java
@@ -2,6 +2,7 @@ package ch.heig_vd.app.converter.parser;
 
 import static org.junit.Assert.*;
 
+import ch.heig_vd.app.converter.interpreter.TemplateInterpreter;
 import ch.heig_vd.app.converter.utils.Metadata;
 import org.apache.commons.io.FileUtils;
 import org.junit.AfterClass;
@@ -9,6 +10,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.*;
+import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -18,101 +20,6 @@ public class PageParserTest {
     public static void setup() {
         File dir = new File("./PageParserTest");
         dir.mkdir();
-    }
-
-    @Test
-    public void metadataShouldBeParsedRetrieved() {
-        try {
-            // Creates test file
-            File input = new File("./PageParserTest/input1.md");
-            String inputContent = "titre:metaTitle\n" +
-                                  "myMetaName:metaContentText\n" +
-                                  "date:metaDate\n" +
-                                  "---\n" +
-                                  "# MarkdownTitle";
-            FileWriter writer = new FileWriter(input);
-            writer.write(inputContent);
-            writer.close();
-
-            // Extracts the metadata
-            ArrayList<Metadata> meta = PageParser.extractMetadata(input);
-
-            // Checks validity
-            assertEquals("titre", meta.get(0).getName());
-            assertEquals("myMetaName", meta.get(1).getName());
-            assertEquals("date", meta.get(2).getName());
-            assertEquals("metaTitle", meta.get(0).getContent());
-            assertEquals("metaContentText", meta.get(1).getContent());
-            assertEquals("metaDate", meta.get(2).getContent());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void exceptionWhenParsingWrongMetadataFields() {
-        try {
-            // Creates test file
-            File input = new File("./PageParserTest/input2.md");
-            String inputContent = "titre:metaTitle\n" +
-                    "auteur:metaAuthor\n" +
-                    "wrongmetadatafilednoseparator\n" +
-                    "---\n" +
-                    "# MarkdownTitle";
-            FileWriter writer = new FileWriter(input);
-            writer.write(inputContent);
-            writer.close();
-
-            // Extracts the metadata
-            ArrayList<Metadata> meta = PageParser.extractMetadata(input);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void exceptionWhenParsingMetadataInFileWithNoEndLine() {
-        try {
-            // Creates test file
-            File input = new File("./PageParserTest/input3.md");
-            String inputContent = "titre:metaTitle\n" +
-                    "auteur:metaAuthor\n" +
-                    "date:metaDate\n" +
-                    "# MarkdownTitle";
-            FileWriter writer = new FileWriter(input);
-            writer.write(inputContent);
-            writer.close();
-
-            // Extracts the metadata
-            ArrayList<Metadata> meta = PageParser.extractMetadata(input);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    @Test
-    public void markdownContentShouldBeParsedAndExtracted() {
-        try {
-            // Creates test file
-            File input = new File("./PageParserTest/input4.md");
-            String inputContent = "titre:metaTitle\n" +
-                    "auteur:metaAuthor\n" +
-                    "mymeta:metaContent\n" +
-                    "---\n" +
-                    "# MarkdownTitle";
-            FileWriter writer = new FileWriter(input);
-            writer.write(inputContent);
-            writer.close();
-
-            // Extracts the markdown content
-            String content = PageParser.extractMarkdownContent(input);
-
-            assertEquals("# MarkdownTitle" + System.lineSeparator(), content);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Test
@@ -184,28 +91,18 @@ public class PageParserTest {
         }
     }
 
-    @Test(expected = RuntimeException.class)
-    public void givenFileShouldNotBeDirectoryForMetaExtraction() {
+    @Test
+    public void givenFileShouldNotBeDirectoryForExtraction() {
         File dir = new File("./PageParserTest");
-        PageParser.extractMetadata(dir);
+        ArrayList<Metadata> meta = new ArrayList<>();
+        assertThrows(RuntimeException.class, () -> PageParser.extractAll(dir, meta));
     }
 
-    @Test(expected = RuntimeException.class)
-    public void givenFileShouldHaveMarkdownExtensionForMetaExtraction() {
+    @Test
+    public void givenFileShouldHaveMarkdownExtensionForExtraction() {
         File file = new File("./PageParserTest/testFile.txt");
-        PageParser.extractMetadata(file);
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void givenFileShouldNotBeDirectoryForMarkdownExtraction() {
-        File dir = new File("./PageParserTest");
-        PageParser.extractMarkdownContent(dir);
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void givenFileShouldHaveMarkdownExtensionForMarkdownExtraction() {
-        File file = new File("./PageParserTest/testFile.txt");
-        PageParser.extractMarkdownContent(file);
+        ArrayList<Metadata> meta = new ArrayList<>();
+        assertThrows(RuntimeException.class, () -> PageParser.extractAll(file, meta));
     }
 
     @AfterClass
