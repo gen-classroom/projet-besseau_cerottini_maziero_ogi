a94c265b0f775e05bae54a54905ca77d4bb0d907
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-05-01 12:23:01 +0200

    Modified test to be more restrictive and refused crahsing when testing unless expected

--- src/test/java/ch/heig_vd/app/BuildTest.java
+++ src/test/java/ch/heig_vd/app/BuildTest.java
@@ -18,8 +18,9 @@ import static org.junit.Assert.*;
 
 public class BuildTest {
     static String root = "./buildTest";
-    static String directoryPath = root +"/mon/site";
+    static String directoryPath = root + "/mon/site";
     static PrintStream original = System.err;
+
     @BeforeClass
     public static void setup() {
         File directoryTest = new File(directoryPath);
@@ -27,7 +28,7 @@ public class BuildTest {
     }
 
     private static void createConfig(String path) throws IOException {
-        File jsonConfig = new File(path+"/config.json");
+        File jsonConfig = new File(path + "/config.json");
         BufferedWriter jsonWriter = new BufferedWriter(new FileWriter(jsonConfig));
         jsonWriter.write("{\n" +
                 "\"title\":\"a\",\n" +
@@ -36,73 +37,8 @@ public class BuildTest {
         jsonWriter.close();
     }
 
-    @Test
-    public void directoryBuildCreated() throws IOException {
-        String path = directoryPath+"/buildTest";
-        File directoryTest = new File(path);
-        directoryTest.mkdirs();
-        File buildDirectory = new File(path + "/build");
-        createConfig(path);
-        new CommandLine(new Main()).execute( "build", path);
-        assertTrue(buildDirectory.exists());
-    }
-
-    @Test
-    public void nonExistentDirectoryShouldThrow() throws IOException {
-        String path = directoryPath+"/doesNotExist";
-        File directoryTest = new File(path);
-
-        File buildDirectory = new File(path+"/build"); //build new directory
-
-        System.setErr(new PrintStream(new OutputStream() {
-            public void write(int b) {
-                //DO NOTHING
-            }
-        }));
-        assertEquals(2,new CommandLine(new Main()).execute( "build", path));
-        System.setErr(original);
-    }
-
-    @Test
-    public void configNotCopied() throws IOException {
-        String path = directoryPath+"/configTest";
-        File directoryTest = new File(path);
-        directoryTest.mkdirs();
-        createConfig(path);
-        File buildDirectory = new File(path+"/build"); //build new directory
-
-        new CommandLine(new Main()).execute( "build", path);
-
-        String[] listOfFiles = buildDirectory.list();
-        assert listOfFiles != null;
-        assertEquals(listOfFiles.length, 0);
-    }
-
-    @Test
-    public void buildNotInBuildDirectory() throws IOException {
-        String path = directoryPath+"/buildNotInBuild";
-        File directoryTest = new File(path);
-        directoryTest.mkdirs();
-        createConfig(path);
-
-        File buildDirectory = new File(path + "/build"); //build new directory
-
-        new CommandLine(new Main()).execute( "build", path);
-
-        String[] listOfFiles = buildDirectory.list();
-        for (String file : listOfFiles) {
-            assertFalse(file.contains("build"));
-        }
-    }
-
-    @Test
-    public void FilesCopied() throws IOException {
-        String path = directoryPath+"/testCopy";
-        File directoryTest = new File(path);
-        directoryTest.mkdirs();
-        createConfig(path);
-
-        File template = new File(directoryTest.getPath() + "/template");
+    private static void createTemplateDirectory(String path) throws IOException {
+        File template = new File(path + "/template");
         template.mkdirs();
         File file1 = new File(path + "/template/default.html");
         PrintWriter writer1 = new PrintWriter(new BufferedWriter(new FileWriter(file1)));
@@ -117,12 +53,79 @@ public class BuildTest {
                 "</html>");
         writer1.flush();
         writer1.close();
+    }
+
+    @Test
+    public void directoryBuildCreated() throws IOException {
+        String path = directoryPath + "/buildTest";
+        File directoryTest = new File(path);
+        directoryTest.mkdirs();
+        File buildDirectory = new File(path + "/build");
+        createConfig(path);
+        createTemplateDirectory(path);
+        assertEquals(0, new CommandLine(new Main()).execute("build", path));
+        assertTrue(buildDirectory.exists());
+    }
+
+    @Test
+    public void nonExistentDirectoryShouldThrow() throws IOException {
+        String path = directoryPath + "/doesNotExist";
+        System.setErr(new PrintStream(new OutputStream() {
+            public void write(int b) {
+                //DO NOTHING
+            }
+        }));
+        assertEquals(2, new CommandLine(new Main()).execute("build", path));
+        System.setErr(original);
+    }
+
+    @Test
+    public void configNotCopied() throws IOException {
+        String path = directoryPath + "/configTest";
+        File directoryTest = new File(path);
+        directoryTest.mkdirs();
+        createConfig(path);
+        createTemplateDirectory(path);
+        File buildDirectory = new File(path + "/build"); //build new directory
+
+        assertEquals(0, new CommandLine(new Main()).execute("build", path));
+
+        String[] listOfFiles = buildDirectory.list();
+        assert listOfFiles != null;
+        assertEquals(listOfFiles.length, 0);
+    }
+
+    @Test
+    public void buildNotInBuildDirectory() throws IOException {
+        String path = directoryPath + "/buildNotInBuild";
+        File directoryTest = new File(path);
+        directoryTest.mkdirs();
+        createConfig(path);
+        createTemplateDirectory(path);
+
+        File buildDirectory = new File(path + "/build"); //build new directory
+        assertEquals(0, new CommandLine(new Main()).execute("build", path));
+
+        String[] listOfFiles = buildDirectory.list();
+        for (String file : listOfFiles) {
+            assertFalse(file.contains("build"));
+        }
+    }
+
+    @Test
+    public void FilesCopied() throws IOException {
+        String path = directoryPath + "/testCopy";
+        File directoryTest = new File(path);
+        directoryTest.mkdirs();
+        createConfig(path);
+        createTemplateDirectory(path);
+
         File newDirectory = new File(directoryTest.getPath() + "/dossier");
         newDirectory.mkdirs();
         File image = new File(newDirectory.getPath() + "/image.png");
         image.createNewFile();
 
-        File input = new File(directoryTest.getPath()+"/input.md");
+        File input = new File(directoryTest.getPath() + "/input.md");
         FileWriter writer = new FileWriter(input);
         String inputContent = "titre:metaTitle\n" +
                 "auteur:metaAuthor\n" +
@@ -132,25 +135,20 @@ public class BuildTest {
         writer.write(inputContent);
         writer.close();
 
-        File buildDirectory = new File( path + "/build");
+        File buildDirectory = new File(path + "/build");
 
-        System.setErr(new PrintStream(new OutputStream() {
-            public void write(int b) {
-                //DO NOTHING
-            }
-        }));
-        new CommandLine(new Main()).execute( "build", directoryTest.getPath());
-        System.setErr(original);
+
+        assertEquals(0, new CommandLine(new Main()).execute("build", directoryTest.getPath()));
         ArrayList<String> list = new ArrayList<String>(Arrays.asList(Objects.requireNonNull(directoryTest.list())));
         ArrayList<String> listBuild = new ArrayList<String>(Arrays.asList(Objects.requireNonNull(buildDirectory.list())));
 
         for (String file : list) {
-            if(file.equals("config.json") || file.equals("build") || file.equals("template")){
+            if (file.equals("config.json") || file.equals("build") || file.equals("template")) {
                 assertFalse(listBuild.contains(file));
-            }else{
-                if (file.substring(file.length() - 3).equals(".md")){
-                    assertTrue(listBuild.contains(file.substring(0, file.length()-2) +"html"));
-                }else{
+            } else {
+                if (file.endsWith(".md")) {
+                    assertTrue(listBuild.contains(file.substring(0, file.length() - 2) + "html"));
+                } else {
                     assertTrue(listBuild.contains(file));
                 }
 
@@ -162,24 +160,24 @@ public class BuildTest {
         ArrayList<String> list2 = new ArrayList<String>(Arrays.asList(Objects.requireNonNull(directoryTest2.list())));
         ArrayList<String> listBuild2 = new ArrayList<String>(Arrays.asList(Objects.requireNonNull(buildDirectory2.list())));
 
-        for (String file : list2){
+        for (String file : list2) {
             assertTrue(listBuild2.contains(file));
         }
     }
 
     @Test
     public void buildShouldThrowWhenNoConfigIsPresent() throws IOException {
-        String path = directoryPath+"/noConfigTest";
+        String path = directoryPath + "/noConfigTest";
         File directoryTest = new File(path);
         directoryTest.mkdirs();
-        File buildDirectory = new File(path+"/build"); //build new directory
+        File buildDirectory = new File(path + "/build"); //build new directory
         // Suppress warning
         System.setErr(new PrintStream(new OutputStream() {
             public void write(int b) {
                 //DO NOTHING
             }
         }));
-        assertEquals(2, new CommandLine(new Main()).execute( "build", path));
+        assertEquals(2, new CommandLine(new Main()).execute("build", path));
         System.setErr(original);
     }
 
