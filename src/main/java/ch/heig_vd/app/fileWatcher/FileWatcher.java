7128f0f5486a491cb0b0a16492837e86d07d89d4
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-05-20 18:42:13 +0200

    File Watcher with recursive following. New directory are not registered correctly for now

--- src/main/java/ch/heig_vd/app/fileWatcher/FileWatcher.java
+++ src/main/java/ch/heig_vd/app/fileWatcher/FileWatcher.java
@@ -1,6 +1,89 @@
 package ch.heig_vd.app.fileWatcher;
 
-public class FileWatcher {
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
 
-    FileWatcher(){}
+public class FileWatcher {
+    private WatchService watchService;
+    private Map<WatchKey, Path> keys;
+
+    public FileWatcher(Path path) throws IOException {
+        watchService = FileSystems.getDefault().newWatchService();
+        keys = new HashMap<WatchKey, Path>();
+        walkAndRegisterDirectories(path);
+    }
+
+
+    public void events() throws InterruptedException {
+
+
+        // wait for key to be signalled
+        WatchKey key;
+        while ((key = watchService.take()) != null) {
+
+
+            Path dir = keys.get(key);
+            if (dir == null) {
+                System.err.println("WatchKey not recognized!!");
+                continue;
+            }
+
+            for (WatchEvent<?> event : key.pollEvents()) {
+                @SuppressWarnings("rawtypes")
+                WatchEvent.Kind kind = event.kind();
+
+                // Context for directory entry event is the file name of entry
+                @SuppressWarnings("unchecked")
+                Path name = ((WatchEvent<Path>) event).context();
+                Path child = dir.resolve(name);
+
+                // print out event
+                System.out.format("%s: %s\n", event.kind().name(), child);
+
+                // if directory is created, and watching recursively, then register it and its sub-directories
+                if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
+                    try {
+                        System.out.println("Adding "+child);
+                        if (Files.isDirectory(child)) {
+                            walkAndRegisterDirectories(child);
+                        }
+                    } catch (IOException x) {
+                        // do something useful
+                    }
+                }
+            }
+
+            // reset key and remove from set if directory no longer accessible
+            boolean valid = key.reset();
+            if (!valid) {
+                keys.remove(key);
+                // all directories are inaccessible
+                if (keys.isEmpty()) {
+                    break;
+                }
+            }
+        }
+
+    }
+
+    private void registerDirectory(Path dir) throws IOException {
+        WatchKey key = dir.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY,
+                StandardWatchEventKinds.ENTRY_DELETE,
+                StandardWatchEventKinds.ENTRY_MODIFY);
+        keys.put(key, dir);
+    }
+
+    private void walkAndRegisterDirectories(final Path start) throws IOException {
+        // register directory and sub-directories
+        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                registerDirectory(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
 }
--- src/test/java/ch/heig_vd/app/fileWatcher/FileWatcherTest.java
+++ src/test/java/ch/heig_vd/app/fileWatcher/FileWatcherTest.java
@@ -1,4 +1,12 @@
 package ch.heig_vd.app.fileWatcher;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class FileWatcherTest {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        FileWatcher watcher = new FileWatcher(Path.of("./test"));
+        watcher.events();
+    }
 }
