4cbbc060ef7f2ce6c0a27e8379ef5275ff6e6e7d
Nicolas Ogi <nicolas.ogi@heig-vd.ch>
2021-05-21 12:42:58 +0200

    Modifying serve command

--- src/main/java/ch/heig_vd/app/command/Serve.java
+++ src/main/java/ch/heig_vd/app/command/Serve.java
@@ -1,51 +1,49 @@
 package ch.heig_vd.app.command;
 
-import com.sun.net.httpserver.Headers;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpServer;
-import org.apache.commons.io.FilenameUtils;
 import picocli.CommandLine;
 
-import java.io.*;
-import java.net.InetSocketAddress;
-import java.net.URI;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 
 @CommandLine.Command(name = "serve",
         description = "Serve a static site")
+
 public class Serve implements Runnable {
     @CommandLine.Parameters(description = "Path of site to serve.")
-    private static String filePath;
+    private static Path site;
+
     private static final int PORT = 8080;
 
     public void run() {
-        try {
-            Path path = Paths.get(filePath, "/build").normalize().toAbsolutePath();
+
+        new CommandLine(new Build()).execute(site.toString());
+
+        // Serve the site
+        Javalin.create(config ->
+                config.addStaticFiles(site.resolve("build").toAbsolutePath().toString(), Location.EXTERNAL)).start(PORT);
+
+
+            /*Path path = Paths.get(site, "/build").normalize().toAbsolutePath();
             if (!path.toFile().exists()) {
                 throw new IllegalArgumentException("Directory does not exists");
             }
-            filePath = path.toString();
+            site = path.toString();
 
             HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", PORT), 0);
             httpServer.createContext("/", new MyHandler());
             httpServer.start(); //to start server
             while (true) ;
-            //httpServer.stop(0); //to stop the server
+            //httpServer.stop(0); //to stop the server*/
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
-    static class MyHandler implements HttpHandler {
+    /*static class MyHandler implements HttpHandler {
         public void handle(HttpExchange ex) throws IOException {
             URI uri = ex.getRequestURI();
             String name = new File(uri.getPath()).getName();
             System.out.println(uri.getPath());
-            File path = new File(filePath, uri.getPath());
+            File path = new File(site, uri.getPath());
             System.out.println("Asking "+name);
             Headers h = ex.getResponseHeaders();
 
@@ -81,5 +79,5 @@ public class Serve implements Runnable {
                     return "text/html";
             }
         }
-    }
+    }*/
 }
