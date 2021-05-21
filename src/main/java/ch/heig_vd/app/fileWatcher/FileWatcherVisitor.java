6335ee6b0155cb063480a340b22dedc2330c63a9
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-05-21 12:18:03 +0200

    Added comment

--- src/main/java/ch/heig_vd/app/fileWatcher/FileWatcherVisitor.java
+++ src/main/java/ch/heig_vd/app/fileWatcher/FileWatcherVisitor.java
@@ -4,5 +4,10 @@ import java.nio.file.Path;
 
 public interface FileWatcherVisitor {
 
+    /**
+     * Visitor for the file Watcher
+     * @param name The name of the file
+     * @param path The path of the file
+     */
     void visit(String name, Path path);
 }
