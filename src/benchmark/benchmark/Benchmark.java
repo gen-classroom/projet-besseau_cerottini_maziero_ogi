f97c1a68fa7c269e0bfa053098c4525391ffef38
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-05-28 11:13:30 +0200

    Modified stuff

--- src/benchmark/benchmark/Benchmark.java
+++ src/benchmark/benchmark/Benchmark.java
@@ -1,25 +1,15 @@
 package benchmark;
-import ch.heig_vd.app.command.Build;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
 
-import java.io.File;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 
 public class Benchmark {
     public static void main(String[] args) throws Exception {
-        org.openjdk.jmh.Main.main(args);
+        Options options = new OptionsBuilder().forks(1).warmupIterations(2).measurementIterations(4).build();
+        new Runner(options).run();
     }
 
-    @org.openjdk.jmh.annotations.Benchmark
-    @BenchmarkMode(Mode.AverageTime)
-    public void test(){
-        String path = "./benchmark/example";
-        File filesDirectory = new File(path.toString()); //get all directory from there
-        File configFile = new File(path+"/config.json");
-        File buildDirectory = new File(path + "/build"); //build new directory
-        buildDirectory.mkdir();
-        File templateFolder = new File(path + "/template");
-        new Build().build(filesDirectory, configFile, buildDirectory, templateFolder);
-    }
+
 }
